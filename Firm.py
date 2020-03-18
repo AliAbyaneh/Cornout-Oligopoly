1cd843129d3555201bcb55c6401d37f0093fefc0
aliabyaneh <abyaneh@outlook.com>
2020-03-17 22:40:41 -0400

    Firm Class created

--- Firm.py
+++ Firm.py
@@ -1,1 +1,19 @@
+class Firm(object):
+    def __init__(self, _Estimator, cost = 0):
+        self.production_cost = cost
+        self.cost_history = []
+        self.production_history =[]
+        self.estimator = _Estimator
 
+    def play_next_round(self):
+        cost_dist = self.estimator.estimate(self.cost_history)
+
+
+    def add_cost(self, cost):
+        self.cost_history.append(cost)
+
+    def add_production_level(self, p):
+        self.production_history.append(p)
+
+    def dynamic_program_solver(self, cost_dist):
+        pass
