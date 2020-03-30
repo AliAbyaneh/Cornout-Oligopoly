7851ee4c58dcfc4fc3625558965b8a48fc92e59c
aliabyaneh <abyaneh@outlool.com>
2020-03-30 00:13:08 -0400

    changes

--- Cartel.py
+++ Cartel.py
@@ -1,12 +1,78 @@
 from Firm import Firm
 import numpy as np
 import scipy.stats as stats
-from scipy.stats import rv_continuous
+import matplotlib.pyplot as plt
 
 class Cartel(object):
-    def __init__(self):
-        pass
+
+    def __init__(self, agents = 4):
+        self.number_of_firms = agents
+        self.firms = []
+
+        self.Equlibrium_production = 14 * self.number_of_firms
+        self.cost_dist = stats.norm(1,0.3)
+
+        for i in range(self.number_of_firms):
+            self.firms.append(Firm(_Estimator=None,
+                                   _cost_dist=self.cost_dist,
+                                   _trigger_price= 10,
+                                   cost = 10,
+                                   n = agents,
+                                   cournout_equilibrium_plevel=self.Equlibrium_production/self.number_of_firms))
 
 
 
+    def cornout_equilibrium(self):
 
+        search_dimension = 20*self.number_of_firms
+        BR = np.zeros(search_dimension)
+        for r in range(search_dimension):
+            BR[r] = self.firms[0].best_response(r)
+        for i,j in zip(BR, np.arange(1, len(BR))):
+            if (self.number_of_firms-1)*i == j:
+                self.Equlibrium_production = i*self.number_of_firms
+                print(i)
+        # print(BR)
+        print(self.firms[0].dynamic_program_solver())
+        fig, ax = plt.subplots()
+        fig.set_size_inches(12, 8, forward=True)
+        ax.plot(np.arange(search_dimension), BR, "-", label = r'$B_{_i}(S_{i})$')
+        ax.plot(BR, np.arange(search_dimension), "-", label = r'$B_{i}(S_{-i})$')
+        ax.legend()
+        plt.xlabel(r'$B_{_i}(S_{i})$')
+        plt.ylabel(r'$B_{i}(S_{-i})$')
+        plt.show()
+
+    def play_game(self, rounds = 10000):
+        production_history = []
+        inverse_demand_history = []
+        for round in range(rounds):
+            print(round)
+            accumulative_production = 0
+            for firm in self.firms:
+                accumulative_production += firm.play_next_round(round)
+
+            production_history.append(accumulative_production)
+            # print(accumulative_production)
+            inverse_demand = np.abs(self.cost_dist.rvs(1))*max(10, 20*self.number_of_firms - accumulative_production)#(np.abs(self.cost_dist.rvs(1))/np.sqrt((10*accumulative_production/self.number_of_firms)))
+            # print(inverse_demand)
+            for firm in self.firms:
+                firm.add_cost(inverse_demand)
+            inverse_demand_history.append(inverse_demand)
+
+        fig, ax = plt.subplots()
+        ax.plot(np.arange(rounds), production_history)
+        plt.show()
+        fig, ax = plt.subplots()
+        fig.set_size_inches(12, 8, forward=True)
+        ax.plot(np.arange(rounds), inverse_demand_history, title="Inverse Demenad Function")
+        plt.xlabel("Inverse Demand")
+        plt.ylabel("Time")
+        plt.show()
+
+if __name__ == "__main__":
+    cartel = Cartel()
+    # print(cartel.firms[0].best_response(14.0*3))
+    # cartel.cornout_equilibrium()
+    #
+    cartel.play_game()
--- Firm.py
+++ Firm.py
@@ -13,23 +13,48 @@ class gaussian_gen(rv_continuous):
 
 class Firm(object):
 
-    def __init__(self, _Estimator, _cost_dist, _trigger_price, df = 0.99, cost = 0, n = 5):
+    def __init__(self, _Estimator, _cost_dist, _trigger_price, df = 0.99, cost = 0, n = 10, cournout_equilibrium_plevel = 14):
         self.production_cost = cost
         self.cost_history = []
+        self.inverse_demand_history = []
         self.production_history =[]
         self.estimator = _Estimator
         self.discount_factor = df
         self.cost_dist = _cost_dist
         self.players = n
         self.trigger_price = _trigger_price
-        self.var_produc_cost = VariableProductionCost([0,0.01],[0,3],250,2)
+        self.var_produc_cost = VariableProductionCost([0,0.01],[0,3],150,2)
+        self.optimal_production = 4
+        self.revisionary = False
+        self.revisionary_counter = 0
+        self.cournout_equilibrium_plevel = cournout_equilibrium_plevel
 
-    def play_next_round(self):
-        pass
+    def play_next_round(self, round):
+        if round > 0 and self.revisionary is False:
+            if (self.inverse_demand_history[-1]) < self.trigger_price:
+                self.revisionary = True
+        if self.revisionary is True:
+            if self.revisionary_counter >= self.find_minimum_revisionary_period(self.optimal_production):
+                self.revisionary_counter = 0
+                self.revisionary = False
+            else:
+                self.revisionary_counter += 1
+                self.production_history.append(self.cournout_equilibrium_plevel)
+                return self.cournout_equilibrium_plevel
+        if round % 100 == 0:
+            if round > 10:
+                print(self.cost_history)
+                param = stats.norm.fit(self.cost_history)
+                self.cost_dist = None
+                self.cost_dist = stats.norm(loc=float(param[0]), scale=float(param[1]))
+            self.optimal_production = self.dynamic_program_solver()
 
+        self.add_production_level(self.optimal_production)
+        return self.optimal_production
 
     def add_cost(self, cost):
-        self.cost_history.append(cost)
+        self.cost_history.append((cost/max(self.production_cost,(20 - self.production_history[-1])*self.players)))
+        self.inverse_demand_history.append(cost)
 
     def add_production_level(self, p):
         self.production_history.append(p)
@@ -41,36 +66,54 @@ class Firm(object):
         search_dimension = 50
         V = np.zeros(search_dimension)
         t = self.find_minimum_revisionary_period(10)
-        for r in range(1,search_dimension):
-                v = V[r] + 1
-                delta_i_r = self.delta_i(r)
-                expected_price_r = self.expected_price(r)
-                while np.abs(v - V[r]) > 1e-3:
-                    v = V[r]
-                    V[r] = delta_i_r + self.discount_factor*(1 - self.cost_dist.cdf(self.trigger_price/expected_price_r))*V[r]
-                    V[r] = V[r] + self.cost_dist.cdf(self.trigger_price/expected_price_r)*(np.sum(np.power(self.discount_factor, np.arange(t))))*self.delta_i(r) + (self.discount_factor**t)*V[r]
-        print(np.argmax(V, axis=0))
-        print(self.expected_price(np.argmax(V, axis=0)))
-        return np.argmax(V, axis=0)
-        # print(np.argmax(V, axis=1))
+
+        revisionary_value = self.delta_i(self.cournout_equilibrium_plevel*self.players)
+        coeff_1 = (self.discount_factor - self.discount_factor**t)
+        coeff_2 = revisionary_value/(1-self.discount_factor)
+
+        for r in range(1, search_dimension):
+            expected_price_r = self.expected_price(self.players*r)
+            normal_round_value = (self.delta_i(r*self.players) - revisionary_value)/(1 - self.discount_factor + coeff_1*self.cost_dist.cdf(self.trigger_price/expected_price_r))\
+                                 + coeff_2
+            # print(expected_price_r)
+            V[r] = normal_round_value
+
+        self.add_production_level(np.max(V))
+        return np.argmax(V)
 
     def find_minimum_revisionary_period(self, r):
         return 10
 
     def expected_price(self, r):
         expected_price = 0
-        for i in range(1,100):
-            expected_price += (1/np.sqrt(r * i)) * (self.cost_dist.cdf(i) - self.cost_dist.cdf(i-1))
+        for i in range(1,200):
+            expected_price += ((i/100)*max(self.production_cost,(20*self.players - r)) * (self.cost_dist.cdf(i/100) - self.cost_dist.cdf((i-1)/100)))
         return expected_price
 
 
-    def delta_i(self, r):
-        return r*(self.expected_price(r) - max(self.production_cost,self.var_produc_cost.estimate_unit_cost(r)))
+    def delta_i(self, r, r_i = 0):
+        return r*(self.expected_price(r+r_i) - max(self.production_cost,0*self.var_produc_cost.estimate_unit_cost(r)))
 
-
+    def best_response(self, r_i):
+        """
+        V_i(r) = E(pi(r, cost(r))) + beta*probability(trigger_price < cost(r))*V_i(r) + probability(trigger_price > cost(r))*(sum(beta^t*delta_i)+beta^T*V_i(r))
+        """
+        search_dimension = 100
+        V = np.zeros(search_dimension)
+        for r in range(1, search_dimension):
+            V[r] = r*(max(self.production_cost,(20*self.players - r - r_i)) -max(self.production_cost,0*self.var_produc_cost.estimate_unit_cost(r)))
+        # print(V)
+        return np.argmax(V)
 
 if __name__ == "__main__":
-    f = Firm(_Estimator=None, _cost_dist=stats.norm(0,10), _trigger_price=0.01, cost = 0.004)
-    f.dynamic_program_solver()
+    f = Firm(_Estimator=None,
+         _cost_dist=stats.norm(1, 0.3),
+         _trigger_price=10,
+         cost=10,
+         n=4,
+         cournout_equilibrium_plevel=10)
+    print(f.expected_price(10))
+    print (f.best_response(1))
+    # print(f.dynamic_program_solver())
 
 
